dc975969fc4ec56076eaec6c46c265a13a40b2b1
szib <szib77@gmail.com>
2019-03-10 23:04:34 +0000

    Super bonus

--- hashketball.rb
+++ hashketball.rb
@@ -174,20 +174,6 @@ def player_with_longest_name
   all_players.max_by { |player, player_data| player.length }[0]
 end
 
-# def good_practices
-#   game_hash.each do |location, team_data|
-#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-#     binding.pry
-#       team_data.each do |attribute, data|
-#         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-#         binding.pry
- 
-#         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-#         data.each do |data_item|
-#             binding.pry
-#       end
-#     end
-#   end
-# end
-
-# good_practices
+def long_name_steals_a_ton?
+  all_players.max_by { |player, player_data| player_data[:steals] }[0] == player_with_longest_name
+end
