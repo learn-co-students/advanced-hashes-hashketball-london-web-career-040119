d7fb93ca5fbf7b0cda95f1483b0efba0d1ec4a0e
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-03-22 18:49:22 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,136 +1,122 @@
 require 'pry'
-def game_hash 
-	game_hash = {
-		:home => 
-		{
-			:team_name => "Brooklyn Nets",
-			:colors => ["Black", "White"],
-			:players => {
-				:player1 => {
-					:name => "Alan Anderson",
-					:number => 0,
-					:shoe_size => 16,
-					:points => 22,
-					:rebounds => 12,
-					:assists => 12,
-					:steals => 3,
-					:blocks => 1,
-					:slam_dunks => 1
-				},
-				:player2 => {
-					:name => "Reggie Evans",
-					:number => 30,
-					:shoe_size => 14,
-					:points => 12,
-					:rebounds => 12,
-					:assists => 12,
-					:steals => 12,
-					:blocks => 12,
-					:slam_dunks => 7
-				},
-				:player3 => {
-					:name => "Brook Lopez",
-					:number => 11,
-					:shoe_size => 17,
-					:points => 17,
-					:rebounds => 19,
-					:assists => 10,
-					:steals => 3,
-					:blocks => 1,
-					:slam_dunks => 15
-				},
-				:player4 => {
-					:name => "Mason Plumlee",
-					:number => 1,
-					:shoe_size => 19,
-					:points => 26,
-					:rebounds => 12,
-					:assists => 6,
-					:steals => 3,
-					:blocks => 8,
-					:slam_dunks => 5
-				},
-				:player5 => {
-					:name => "Jason Terry",
-					:number => 31,
-					:shoe_size => 15,
-					:points => 19,
-					:rebounds => 2,
-					:assists => 2,
-					:steals => 4,
-					:blocks => 11,
-					:slam_dunks => 1
-		
-				}
-		},		
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: {
+        "Alan Anderson" => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        "Reggie Evans" => {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        "Brook Lopez" => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        "Mason Plumlee" => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        "Jason Terry" => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      }
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: {
+        "Jeff Adrien" => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        "Bismak Biyombo" => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        "DeSagna Diop" => {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        "Ben Gordon" => {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        "Brendan Haywood" => {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      }
+    }
+  }
+end
 
-		:away =>
-		{
-			:team_name => "Charlotte Hornets",
-			:colors => ["Turquoise", "Purple"],
-			:players => {
-				:player1 => {
-					:name => "Jeff Adrien",
-					:number => 4,
-					:shoe_size => 18,
-					:points => 10,
-					:rebounds => 1,
-					:assists => 1,
-					:steals => 2,
-					:blocks => 7,
-					:slam_dunks => 2
-				},
-				:player2 => {
-					:name => "Bismak Biyombo",
-					:number => 0,
-					:shoe_size => 16,
-					:points => 12,
-					:rebounds => 4,
-					:assists => 7,
-					:steals => 7,
-					:blocks => 15,
-					:slam_dunks => 10
-				},
-				:player3 => {
-					:name => "DeSagna Diop",
-					:number => 2,
-					:shoe_size => 14,
-					:points => 24,
-					:rebounds => 12,
-					:assists => 12,
-					:steals => 4,
-					:blocks => 5,
-					:slam_dunks => 5
-				},
-				:player4 => {
-					:name => "Ben Gordon",
-					:number => 8,
-					:shoe_size => 15,
-					:points => 33,
-					:rebounds => 3,
-					:assists => 2,
-					:steals => 1,
-					:blocks => 1,
-					:slam_dunks => 0
-				},
-				:player5 => {
-					:name => "Brendon Haywood",
-					:number => 33,
-					:shoe_size => 15,
-					:points => 6,
-					:rebounds => 12,
-					:assists => 12,
-					:steals => 22,
-					:blocks => 5,
-					:slam_dunks => 12
-				}
-			}
-		}
-	}
-}
-end 
 
-# Write your code here!
 def big_shoe_rebounds(h)
   #will return the number of rebounds associated with the player that has the largest shoe size
 	# STEP1: find the player with the biggest shoe size
